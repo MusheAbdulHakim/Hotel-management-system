42dfda82296bcfe755683a42161ab5ad38bfafcf
MusheAbdulHakim <musheabdulhakim99@gmail.com>
2020-12-30 02:13:23 +0000

    login

--- login.php
+++ login.php
@@ -1,64 +1,12 @@
 ﻿<?php
-	
- session_start();
-//Database Configuration File
-include_once('includes/config.php');
-error_reporting(0);
-if(isset($_POST['login']))
-  {
- 
-    // Getting username and password
-    $uname=$_POST['username'];
-    $password=$_POST['password'];
-    // Fetch data from database on the basis of username/email and password
-    $sql ="SELECT UserName,Password FROM tbladmin WHERE (UserName=:usname)";
-    $query= $dbh -> prepare($sql);
-    $query-> bindParam(':usname', $uname, PDO::PARAM_STR);
-    $query-> execute();
-    $results=$query->fetchAll(PDO::FETCH_OBJ);
-				if($query->rowCount() > 0)
-				{
-				foreach ($results as $row) {
-				$hashpass=$row->Password;
-				}
-				if(!empty($_POST["remember"])) {
-			//COOKIES for username
-			setcookie ("user_login",$_POST["username"],time()+ (10 * 365 * 24 * 60 * 60));
-			//COOKIES for password
-			setcookie ("userpassword",$_POST["password"],time()+ (10 * 365 * 24 * 60 * 60));
-			} else {
-					if(isset($_COOKIE["user_login"])) {
-						setcookie ("user_login","");
-					if(isset($_COOKIE["userpassword"])) {
-						setcookie ("userpassword","");
-					}
-				}
-			}
-			//verifying Password
-			if (password_verify($password, $hashpass)) {
-					$_SESSION['userlogin']=$_POST['username'];
-    echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
-  } else {
-  	$wrongpassword='  <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>Oh Snapp!😕</strong> Alert <b class="alert-link">Password: </b>You entered wrong password.
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        </div>'; 
-  			}
+	require_once 'includes/library.php';
+	$app = new HotelLib();
+	if(isset($_POST['login'])){
+		global $wrongpassword,$wrongusername;
+		$username = htmlspecialchars($_POST['username']);
+		$password = htmlspecialchars($_POST['password']);
+		$app->Login($username,$password);
 	}
-//if username or email not found in database
-else{
-	$wrongusername='
-      <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>Oh Snapp!🙃</strong> Alert <b class="alert-link">UserName: </b> You entered a wrong UserName.
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>';
-  }
- 
-}
 ?>
 <!DOCTYPE html>
 <html>
@@ -93,12 +41,12 @@ else{
 						<input class="input100" type="text" name="username" placeholder="Username">
 						<span class="focus-input100" data-placeholder="&#xf207;"></span>
 					</div>
-					<span><?php echo $wrongusername; ?></span>
+					
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
 						<input class="input100" type="password" name="password" placeholder="Password">
 						<span class="focus-input100" data-placeholder="&#xf191;"></span>
 					</div>
-					<span><?php echo $wrongpassword; ?></span>
+					
 					<div class="contact100-form-checkbox">
 						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember" <?php if(isset($_COOKIE["user_login"])) { ?> checked <?php } ?>>
 						<label class="label-checkbox100" for="ckb1">
