71d9f491bec2df13f6982a98f0cebbc119462c66
MusheAbdulHakim <musheabdulhakim99@gmail.com>
2020-12-30 00:55:59 +0000

    templates

---
+++ templates/footer.php
@@ -0,0 +1,19 @@
+	<div class="page-footer">
+			<div class="page-footer-inner">
+				<span>Copyright &copy;</span>
+				<span id="copyright">
+      <script>
+        document
+          .getElementById("copyright")
+          .appendChild(
+            document.createTextNode(new Date().getFullYear())
+          );
+      </script></span>      
+				<a class="text-bold-800 grey darken-2" href="https://www.linkedin.com/in/yahuza-mushe-abdul-hakim-2155351a9/"
+        target="_blank">Mushe Abdul-Hakim </a>, All rights reserved.</span>
+      
+			</div>
+			<div class="scroll-to-top">
+				<i class="icon-arrow-up"></i>
+			</div>
+		</div>
