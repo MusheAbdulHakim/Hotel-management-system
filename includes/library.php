43ed7e92e6517ecb2e44e2906e532dcf961a9112
MusheAbdulHakim <musheabdulhakim99@gmail.com>
2020-12-30 00:56:15 +0000

    app library

---
+++ includes/library.php
@@ -0,0 +1,26 @@
+<?php 
+	require_once __DIR__ 'config.php';
+
+	class HotelApp(){
+
+			/*
+       * Login
+       *
+       * @param $username, $password
+       * @return $mixed
+       * */
+
+       /*
+    * Login
+    *
+    * @param $username, $password
+    * @return $mixed
+    * */  
+     public function Login($username,$password){
+
+     }
+
+
+	}
+
+?>
